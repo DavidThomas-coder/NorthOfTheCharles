aa99b21bb283801298453f07eb2407b2d3d2e6f9
RipNasty <det625@gmail.com>
2023-06-22 16:02:12 -0400

    made the posts migration

---
+++ server/src/db/migrations/20230622195707_createPosts.cjs
@@ -0,0 +1,23 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.createTable("posts", (table) => {
+        table.bigIncrements("id")
+        table.string("type").notNullable()
+        table.string("mediaURL").notNullable()
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+    })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+    return knex.schema.dropTableIfExists("posts")
+}
