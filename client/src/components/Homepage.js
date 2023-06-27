de071c440a018519e383be9a4e020d4855c42410
Todd Garrison <toddlgarrison@gmail.com>
2023-06-27 13:36:40 -0400

    Add homepage

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = (props) => {
+    return (
+        <div>
+            <h1>North of the Charles</h1>
+            <h4>Post Your Northern Views</h4>
+
+            <div className='developed-by'>
+                <p>Developed by David Thomas, Solomon Montagno, and Todd Garrison</p>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
