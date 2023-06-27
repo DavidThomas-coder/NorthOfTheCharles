db6c69ce19b99d7fc89ad64c619d9e27f8db63d9
Todd Garrison <toddlgarrison@gmail.com>
2023-06-27 11:52:46 -0400

    Add Comment Model

---
+++ server/src/models/Comment.js
@@ -0,0 +1,34 @@
+const Models = require('./models');
+
+class Comment extends Model {
+    static get tableName() {
+        return 'comments';
+    }
+
+    static get relationMappings() {
+        const { Post, User } = require('./index.js');
+        
+        return {
+            user: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'comments.userId',
+                    to: 'users.id'
+                }
+            },
+
+            post: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Models.Post,
+                join: {
+                    from: 'comments.postId',
+                    to: 'posts.id'
+                }
+            }
+        };
+    }
+
+}
+
+module.exports = Comment;
