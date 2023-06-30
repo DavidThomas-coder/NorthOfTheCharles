9cfb22b392748b35a549f1a8fa2945680107247d
RipNasty <det625@gmail.com>
2023-06-30 14:10:57 -0400

    images on there, fix background color

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,8 +4,14 @@ import { Link } from'react-router-dom';
 const Homepage = (props) => {
     return (
         <div>
-            <h1>North Of The Charles</h1>
-            <h4>Post Your Northern Views</h4>
+            <div className="cell small-12 grid-x align-middle">
+                <div className="cell small-6">
+                    <img src="https://i.imgur.com/nUIsZWu.png" alt="river image" />
+                </div>
+                <div className="cell small-6">
+                    <img src="https://i.imgur.com/j90TSrY.png" alt="notc info" style={{ marginTop: '-20px' }} />
+                </div>
+            </div>
 
             <div className='developed-by'>
                 <p><Link to="/developers">Developed</Link> by David Thomas, Solomon Montagno, and Todd Garrison</p>
