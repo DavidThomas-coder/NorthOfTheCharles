f5af87d8e1bcdb8aa210af627278d91e72ed1034
RipNasty <det625@gmail.com>
2023-07-06 12:44:14 -0400

    made a PostTile

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from'react-router-dom';
+import PostTile from './PostTile';
 
 const Homepage = (props) => {
     const [homeShow, setHomeShow] = useState({
---
+++ client/src/components/PostTile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PostTile = (props) => {
+    const { post, user } = props;
+
+    return (
+        <div className="post-tile">
+            <div className="post-box">
+                {user && (
+                <div className="user-info">
+                    <p>{user.firstName}</p>
+                </div>
+                )}
+                <p className="post-media">{post.mediaURL}</p>
+                <p className="post-body">{post.postBody}</p>
+            </div>
+            </div>
+        );
+};
+
+export default PostTile;
