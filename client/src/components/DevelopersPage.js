c2bbf54f9080786cf6b9dd328f1deda936aefebd
RipNasty <det625@gmail.com>
2023-07-07 21:58:49 -0400

    added my description

--- client/src/components/DevelopersPage.js
+++ client/src/components/DevelopersPage.js
@@ -5,7 +5,7 @@ const DeveloperPage = () => {
         <div>
             <h1>Meet the Developers</h1>
             <h4>David Thomas</h4>
-            <p>Sweet description</p>
+            <p>David is a data expert, creative leader and standup comedian based in Somerville, MA</p>
             <h4>Solomon Montagno</h4>
             <p>Cool beans</p>
             <h4>Todd Garrison</h4>
